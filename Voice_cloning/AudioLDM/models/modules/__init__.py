5b2e0d5242a48ee4582285b2ebf9f0b45249fb4f
Prem-Chand-Jala <123394921+Prem-Chand-Jala@users.noreply.github.com>
2025-01-02 01:29:08 -0500

    Delete Voice_cloning directory

--- Voice_cloning/AudioLDM/models/modules/__init__.py
+++
@@ -1,4 +0,0 @@
-from models.modules.norm import RMSNorm, ConvRMSNorm, LayerNorm
-from models.modules.transformer import ConditionableTransformer
-from models.modules.conformer import ConformerConvBlock
-from models.modules.conv import MaskedConv1d
